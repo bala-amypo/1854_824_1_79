f5ca2e73d3b0b1f9a4612c101437a2af14fe1a17
bala-amypo <bala@amypo.in>
2025-12-18 05:37:54 +0000

    User commit

--- src/main/java/com/example/demo/service/VechicleService.java
+++ src/main/java/com/example/demo/service/VechicleService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Vechicle;
 
-
+import java.util.List;
 
 public interface VechicleService{
     Vechicle saveVechicle(Vechicle vechicle);
