fa41fc358ae98150600f132ec42aa27c2c720277
bala-amypo <bala@amypo.in>
2025-12-21 16:52:00 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VehicleImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleImpl.java
@@ -8,16 +8,20 @@ import com.example.demo.service.VehicleService;
 @Service
 public class VehicleImpl implements VehicleService {
 
-    private final VehicleRepository vechicleRepository;
+    private final VehicleRepository vehicleRepository;
     private final UserRepository userRepository;
 
-    public VehicleImpl(VehicleRepository repository, UserRepository userRepository) {
-        this.vechiRepository = vechicleRepository;
-        this.userRepository=userRepository
+    public VehicleImpl(VehicleRepository vehiclerepository, UserRepository userRepository) {
+        this.vehicleRepository = vehicleRepository;
+        this.userRepository=userRepository;
     }
 
     @Override
     public Vehicle saveVechicle(Vehicle vehicle) {
+        if(vehicle.getCapacity()==null||vehicle.getCapacity()<=0){
+            throw new IllegalArgumentException("capacity");
+        }
+        User user=user
         return repository.save(vehicle);
     }
 }
