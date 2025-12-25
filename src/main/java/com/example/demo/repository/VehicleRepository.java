0d7bb79724ccfbaa891020bca49a55ebd8c8cd27
bala-amypo <bala@amypo.in>
2025-12-25 11:00:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     Optional<Vehicle> findByVehicleNumber(String vehicleNumber);
-
-    List<Vehicle> findByUserId(Long userId);   // REQUIRED
+    List<Vehicle> findByUserId(Long userId);
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,8 +5,6 @@ import com.example.demo.entity.User;
 public interface UserService {
 
     User register(User user);
-
-    User registerUser(User user);     // REQUIRED BY AuthController
-
-    User findByEmail(String email);   // REQUIRED BY AuthController
+    User registerUser(User user);
+    User findByEmail(String email);
 }
